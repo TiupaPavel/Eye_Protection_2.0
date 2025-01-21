6b59c3ec562fc059df2dba0961cd417f7b886912
PAVELLORDS <90398815+TiupaPavel@users.noreply.github.com>
2025-01-21 19:08:07 +0200

    Eye_Protection_2.0

---
+++ Eye Protection Code/Form3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eye_Protection
+{
+    public partial class Form3 : Form
+    {
+        public Form3(Task_9.Form1 form1)
+        {
+            InitializeComponent();
+        }
+    }
+}
